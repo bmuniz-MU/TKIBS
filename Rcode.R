2d11f799f9df0944baaab8002a28917f433eb422
Belen Muñiz <bmuniz@mondragon.edu>
2021-05-07 09:20:26 +0200

    Añado el código de carga de datos

--- Rcode.R
+++ Rcode.R
@@ -4,16 +4,8 @@ install.packages("lubridate");
 library(lubridate)
 install.packages("dplyr"); 
 library(dplyr)
+install.packages("xlsx")
+library(xlsx)
 
 DF <- read_excel("C:/Users/bmuniz/Desktop/Ejercicio Git/Owin_Covid_Data.xlsx")
 str(DF)
-
-DF$date <- ymd(DF$date, tz=NULL)
-class(DF$date)
-
-fecha_max <- max(DF$date)
-fecha_min <- min(DF$date)
-
-totales <- DF %>%
-  group_by (iso_code, location) %>%
-  summarize(total_cases = sum(new_cases))
