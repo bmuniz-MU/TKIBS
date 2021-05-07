20d913528874ed5d5ff27817b15ed81c64243fd6
Belen Muñiz <bmuniz@mondragon.edu>
2021-05-07 09:42:26 +0200

    Modificamos el fichero README.md y generamos datos agregados

--- Rcode.R
+++ Rcode.R
@@ -7,5 +7,17 @@ library(dplyr)
 install.packages("xlsx")
 library(xlsx)
 
-DF <- read_excel("C:/Users/bmuniz/Desktop/Ejercicio Git/Owin_Covid_Data.xlsx")
+DF <- read_excel("C:/Users/bmuniz/Desktop/Ejercicio_Git/Owin_Covid_Data.xlsx")
 str(DF)
+
+DF$date <- ymd(DF$date, tz=NULL)
+class(DF$date)
+
+fecha_max <- max(DF$date)
+fecha_min <- min(DF$date)
+
+totales <- DF %>%
+  group_by (iso_code, location) %>%
+  summarize(total_cases = sum(new_cases))
+
+write_xlsx(totales, "C:/Users/bmuniz/Desktop/Ejercicio_Git/totales.xlsx")
